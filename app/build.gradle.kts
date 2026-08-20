e40ece509d35ac494f427ec14aa0129f16d5b738
ci <example@example.com>
2026-08-20 23:54:41 +0800

    release: bump version to 1.0.0 (versionCode 100)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
     defaultConfig {
         applicationId = "app.komikku"
 
-        versionCode = 81
-        versionName = "1.14.1"
+        versionCode = 100
+        versionName = "1.0.0"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
